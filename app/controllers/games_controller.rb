c04ab6d277934486787340b9a35237df0d9fe360
Siel Goldofsky <sielgold@gmail.com>
2019-11-17 18:57:40 +0200

    unfinished 3

---
+++ app/controllers/games_controller.rb
@@ -0,0 +1,27 @@
+require 'open-uri'
+
+class GamesController < ApplicationController
+  def new
+    @letters = []
+    @sample = 10.times { @letters << ('A'..'Z').to_a.sample }
+  end
+
+  def score
+    @letters = params[:letters].split
+    @word = (params[:word] || "").upcase
+    @included = included?(@word, @letters)
+    @english_word = english_word?(@word)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
+  end
+end
